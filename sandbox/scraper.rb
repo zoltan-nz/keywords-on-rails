85135cbe550338a33e5e8543ed2c1b17fc5270f5
Zoltan Debre <zdebre@gmail.com>
2012-12-10 20:49:23 +0000

    scraper

--- sandbox/scraper.rb
+++ sandbox/scraper.rb
@@ -3,11 +3,11 @@ require 'mechanize'
 require 'pp'
 
 def check(keyword)
-	website_url = URI::encode("http://www.google.com/search?q=#{keyword}")
-	agent = Mechanize.new
-	page = agent.get(website_url)
-	pp page
-	pp page.search('/body')
+  website_url = URI::encode("http://www.google.com/search?q=#{keyword}")
+  agent = Mechanize.new
+  page = agent.get(website_url)
+  #pp page
+  pp page.links
 end
 
 check('tax specialist san francisco')
