92b7cf749a470c7780bd8f49f210951b5a98de3a
Zoltan Debre <zdebre@gmail.com>
2012-12-10 19:04:01 +0000

    scraper

---
+++ sandbox/scraper.rb
@@ -0,0 +1,13 @@
+require 'open-uri'
+require 'mechanize'
+require 'pp'
+
+def check(keyword)
+	website_url = URI::encode("http://www.google.com/search?q=#{keyword}")
+	agent = Mechanize.new
+	page = agent.get(website_url)
+	pp page
+	pp page.search('/body')
+end
+
+check('tax specialist san francisco')
