e7a652216238912da0d47ab8db49b02d3c8c66f4
Zoltan Debre <zdebre@gmail.com>
2012-12-10 23:43:45 +0000

    scraper

--- sandbox/scraper.rb
+++ sandbox/scraper.rb
@@ -19,12 +19,20 @@ begin
 
   con = Mysql2::Client.new(:host => 'localhost', :username => 'root', :password => '', :database => 'keywords')
 
-  con.query("SELECT * FROM keyword_stat WHERE topten = 0").each do |row|
+  result = ""
+
+  con.query("SELECT * FROM keyword_stat WHERE topten = '0'").each do |row|
     id = row["id"]
-    result = check(row["keyword"])
+    time = Benchmark.measure do
+      result = check(row["keyword"])
+    end
+    puts "Time of check: #{time}"
     puts "#{id}. #{row["keyword"]} ==> #{result}"
     query_string = "UPDATE keyword_stat SET intopten = '#{result}', topten = '1' WHERE id LIKE '#{id}'"
-    con.query(query_string)
+    time = Benchmark.measure do
+      con.query(query_string)
+    end
+    puts "Time of query: #{time}"
   end
 
 rescue Mysql2::Error => e
